5e0fc52f2fb9bfd841a25035640db4f275ac084b
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-10-31 14:05:42 -0400

    create file to hold all constant data

---
+++ src/db.js
@@ -0,0 +1,233 @@
+import A_major from './images/A Maj.png';
+import A_minor from './images/A Min.png';
+import Ab_major from './images/Ab Maj.png';
+import Ab_minor from './images/Ab Min.png';
+
+import B_major from './images/B Maj.png';
+import B_minor from './images/B Min.png';
+import Bb_major from './images/Bb Maj.png';
+import Bb_minor from './images/Bb Min.png';
+
+import C_major from './images/C Maj.png';
+import C_minor from './images/C Min.png';
+import Cs_major from './images/C# Maj.png';
+import Cs_minor from './images/C# Min.png';
+
+import D_major from './images/D Maj.png';
+import D_minor from './images/D Min.png';
+
+import E_major from './images/E Maj.png';
+import E_minor from './images/E Min.png';
+import Eb_major from './images/Eb Maj.png';
+import Eb_minor from './images/Eb Min.png';
+
+import F_major from './images/F Maj.png';
+import F_minor from './images/F Min.png';
+import Fs_major from './images/F# Maj.png';
+import Fs_minor from './images/F# Min.png';
+
+import G_major from './images/G Maj.png';
+import G_minor from './images/G Min.png';
+
+export const chords = {
+  AMJ: {
+    name: 'A MAJOR',
+    pianoImage: A_major,
+    // nameImage: placeholder,
+  },
+  AMN: {
+    name: 'A MINOR',
+    pianoImage: A_minor,
+    // nameImage: placeholder,
+  },
+  AFMJ: {
+    name: 'A♭ MAJOR',
+    pianoImage: Ab_major,
+    // nameImage: placeholder,
+  },
+  AFMN: {
+    name: 'A♭ MINOR',
+    pianoImage: Ab_minor,
+    // nameImage: placeholder,
+  },
+  BMJ: {
+    name: 'B MAJOR',
+    pianoImage: B_major,
+    // nameImage: placeholder,
+  },
+  BMN: {
+    name: 'B MINOR',
+    pianoImage: B_minor,
+    // nameImage: placeholder,
+  },
+  BFMJ: {
+    name: 'B♭ MAJOR',
+    pianoImage: Bb_major,
+    // nameImage: placeholder,
+  },
+  BFMN: {
+    name: 'B♭ MINOR',
+    pianoImage: Bb_minor,
+    // nameImage: placeholder,
+  },
+  CMJ: {
+    name: 'C MAJOR',
+    pianoImage: C_major,
+    // nameImage: placeholder,
+  },
+  CMN: {
+    name: 'C MINOR',
+    pianoImage: C_minor,
+    // nameImage: placeholder,
+  },
+  CSMJ: {
+    name: 'C♯ MAJOR',
+    pianoImage: Cs_major,
+    // nameImage: placeholder,
+  },
+  CSMN: {
+    name: 'C♯ MINOR',
+    pianoImage: Cs_minor,
+    // nameImage: placeholder,
+  },
+  DMJ: {
+    name: 'D MAJOR',
+    pianoImage: D_major,
+    // nameImage: placeholder,
+  },
+  DMN: {
+    name: 'D MINOR',
+    pianoImage: D_minor,
+    // nameImage: placeholder,
+  },
+  EMJ: {
+    name: 'E MAJOR',
+    pianoImage: E_major,
+    // nameImage: placeholder,
+  },
+  EMN: {
+    name: 'E MINOR',
+    pianoImage: E_minor,
+    // nameImage: placeholder,
+  },
+  EFMJ: {
+    name: 'E♭ MAJOR',
+    pianoImage: Eb_major,
+    // nameImage: placeholder,
+  },
+  EFMN: {
+    name: 'E♭ MINOR',
+    pianoImage: Eb_minor,
+    // nameImage: placeholder,
+  },
+  FMJ: {
+    name: 'F MAJOR',
+    pianoImage: F_major,
+    // nameImage: placeholder,
+  },
+  FMN: {
+    name: 'F MINOR',
+    pianoImage: F_minor,
+    // nameImage: placeholder,
+  },
+  FSMJ: {
+    name: 'F♯ MAJOR',
+    pianoImage: Fs_major,
+    // nameImage: placeholder,
+  },
+  FSMN: {
+    name: 'F♯ MINOR',
+    pianoImage: Fs_minor,
+    // nameImage: placeholder,
+  },
+  GMJ: {
+    name: 'G MAJOR',
+    pianoImage: G_major,
+    // nameImage: placeholder,
+  },
+  GMN: {
+    name: 'G MINOR',
+    pianoImage: G_minor,
+    // nameImage: placeholder,
+  },
+};
+
+export const akaJon = [
+  'Jonny Boy',
+  'Jon Jon',
+  'Pappa Jon',
+  'Jno',
+  'Piano Man',
+  'Jonnie Wonder',
+  'you Jon Lord wannabe',
+  'JonE',
+];
+
+export const randomPhrases = [
+  'That piano wont play itself kiddo, try this:',
+  'You are neglecting your keys, try this out:',
+  'Life is like a sewer… you get out what you put in, time to do some putting-in:',
+  'The road to success is dotted with many tempting parking space, keep driving my friend:',
+  'Do or do not, there is no try. Do:',
+  'The o Bnly place SUCCESS comes before WORK is the dictionary, now bang this out:',
+  "We talkin' 'bout practice?... Yes, yes we are:",
+  "Dairy cows don't get a day off, neither do you:",
+  "Show me a guy who's afraid to look bad, and I'll show you a guy you can beat every time, you wanna be beat?:",
+  "The best things in life are actually really expensive, so was that piano, get playin':",
+  "If the world didn't suck we'd all fly into space. Before that happens, play this:",
+  "Even if you are on the right track, you'll get run over if you just sit there, jump to it:",
+  'If you think you are too small to be effective, you have never been in the dark with a mosquito... go be that mosquito:',
+  "You're only given a little spark of madness. You mustn't lose it:",
+  "Taking a step backward after taking a step forward is not a disaster, it's more like a cha-cha, let's dance twinkle toes:",
+  "There are two ways to pass a hurdle: leaping over or plowing through… let's monster truck this shit:",
+  "There's a fine line between genius and insanity. Let's erase that line, play this:",
+  "Failure is the condiment that gives success its flavor, let's spice this shit up:",
+  "We don't stop playing because we grow old; we grow old because we stop playing, so play!:",
+  "'Mellifluous' is a sound that is pleasingly smooth and musical to hear, let's see what you got:",
+  'People are more creative in the shower.',
+  'Australia is wider than the moon.',
+  "It's illegal to own just one guinea pig in Switzerland.",
+  'The unicorn is the national animal of Scotland.',
+  'Nutmeg is a hallucinogen.',
+  "The Japanese word 'Kuchi zamishi' is the act of eating when you're not hungry because your mouth is lonely.",
+  "You can hear a blue whale's heartbeat from over 2 miles away.",
+  'The actors who voiced Mickey and Minnie mouse got married in real life.',
+  "The last letter added to the English alphabet was 'J'.",
+  "'Ultracrepidarian' is the word for someone giving an opinion on something they know nothing about.",
+  'A jiffy is an actual unit of time.',
+  "You can't hum if you hold your nose.",
+  'The severed head of a sea slug can grow a whole new body.',
+  "The feeling of getting lost inside a mall is known as the 'Gruen transfer'.",
+  'You lose up to 30 percent of your taste buds during a flight.',
+  "Rabbits can't puke.",
+  'Marie Curie is the only person to earn a Nobel prize in two different sciences.',
+  'The English word with the most definitions is "set."',
+  'Creedence Clearwater Revival has the most No. 2 Billboard hits—without ever hitting No. 1.',
+  'The healthiest place in the world is in Panama.',
+  'Playing the accordion was once required for teachers in North Korea.',
+  'Water makes different pouring sounds depending on its temperature.',
+  "Sandy Island, an island off the coast of Australia was shown on maps for 100 years, but it doesn't actually exist.",
+  'Pigs are constitutionally protected in Florida.',
+  'You can sneeze faster than a cheetah can run.',
+  'The fire hydrant patent was lost in a fire.',
+  'In Germany, people help toads cross the road.',
+  'France has a dozen time zones.',
+  'Men who are feeling stressed out prefer women with curvier figures.',
+  'Shaq only ever made one three-pointer.',
+  'Germany uncovers 2,000 tons of unexploded bombs every year.',
+  "Every time you shuffle a deck of cards, you get a combination that's never existed.",
+  'Actor Bill Murray uses a 1-800 number instead of an agent or manager.',
+  "There's a device that creates energy from the static electricity of snowfall.",
+  'Beaver bum goo is occasionally used to enhance vanilla flavorings.',
+  'The word "MacGyvered" is in the Oxford English Dictionary.',
+];
+
+export const flat = '♭';
+export const sharp = '♯';
+
+export const randomPick = (arr) => {
+  const getIndex = Math.floor(Math.random() * arr.length);
+  const selected = arr[getIndex];
+
+  return selected;
+};
