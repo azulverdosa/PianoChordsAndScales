43df3f170f173ee7fe9758be5e8eca29aada9de0
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-01 12:48:44 -0400

    style button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,22 @@
-const Button = ({ disabled, handleStartClick, start }) => {
+const Button = ({ disabled, handleStartClick, start, style }) => {
   return (
-    <div style={{ display: 'flex', margin: '20px' }}>
-      <button
-        className="ui grey basic button"
-        disabled={disabled}
-        onClick={() => {
-          handleStartClick();
-          console.log('button start value :>> ', start);
-        }}
-      >
-        {start ? 'Stop' : 'Start'}
-      </button>
-    </div>
+    <button
+      className="ui grey basic button"
+      disabled={disabled}
+      onClick={() => {
+        handleStartClick();
+        console.log('button start value :>> ', start);
+      }}
+      style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexGrow: 0,
+        justifyContent: 'center',
+        ...style,
+      }}
+    >
+      {start ? 'Stop' : 'Start'}
+    </button>
   );
 };
 
