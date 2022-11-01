32439dec44b69d5e270eb095daa0dd3e76d1332a
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-01 12:48:10 -0400

    handle state for Button and Slider components, display components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import Home from './components/Home';
-import ChordCard from './components/ChordCard';
+import { useState } from 'react';
+import Button from './components/Button';
+import Card from './components/Card';
+import Slider from './components/Slider';
 
 function App() {
+  const [start, setStart] = useState(false);
+  const [intervalSeconds, setIntervalSeconds] = useState(15);
+
+  const handleStartClick = () => {
+    setStart(!start);
+    console.log('app START');
+  };
+
   return (
-    <div>
-      {/* <Home /> */}
-      <ChordCard />
-    </div>
+    <main style={{ margin: '0 auto', width: '520px' }}>
+      <span
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: '0 80px',
+        }}
+      >
+        <Button
+          disabled={!intervalSeconds}
+          handleStartClick={handleStartClick}
+          start={start}
+          style={{
+            width: '75px',
+          }}
+        />
+        <Slider value={intervalSeconds} onChange={setIntervalSeconds} />
+      </span>
+      <Card intervalSeconds={intervalSeconds} start={start} />
+    </main>
   );
 }
 
